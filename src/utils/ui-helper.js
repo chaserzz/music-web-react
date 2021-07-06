f2c95e76e2e83dda39b8ae68fc533cadc15e3fce
chaserzz <467540052@qq.com>
2021-07-06 22:28:21 +0800

    fix r    ReadMe文件描述更新

--- src/utils/ui-helper.js
+++ src/utils/ui-helper.js
@@ -1,11 +1,16 @@
 export function scrollTo(element, to, duration) {
-  if (duration <= 0) return;
+  if (duration <= 0) {
+    return
+  }
+  console.log(element.scrollTop);
   var difference = to - element.scrollTop;
-  var perTick = difference / duration * 10;
+  var perTick = difference / duration * 10; 
   //多次调用scrollTo 以做到滚动的效果，如果一次性赋值到To会没有滚动的效果
   setTimeout(function() {
       element.scrollTop = element.scrollTop + perTick;
-      if (element.scrollTop === to) return;
+      if (element.scrollTop === to) {
+        return 
+      }
       scrollTo(element, to, duration - 10);
   }, 10);
 }
